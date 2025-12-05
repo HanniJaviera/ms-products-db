81714beffd72a4d357e917d6266d8039b20e0007
HanniJaviera <javiera.arc@hotmail.com>
2025-12-04 22:07:23 -0300

    ajuste UsuarioController

--- src/main/java/cl/duoc/ms_products_db/controller/UsuarioController.java
+++ src/main/java/cl/duoc/ms_products_db/controller/UsuarioController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import cl.duoc.ms_products_db.model.entities.Usuario;
 import cl.duoc.ms_products_db.service.UsuarioService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/usuarios")
-@CrossOrigin(origins = "*")
 public class UsuarioController {
 
     @Autowired
