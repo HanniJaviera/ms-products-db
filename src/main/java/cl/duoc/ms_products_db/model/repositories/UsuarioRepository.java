bae35bcaf14de8885b9ab26f0a292e895187901e
HanniJaviera <javiera.arc@hotmail.com>
2025-12-04 21:57:51 -0300

    Se agrega usuario al back

---
+++ src/main/java/cl/duoc/ms_products_db/model/repositories/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package cl.duoc.ms_products_db.model.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cl.duoc.ms_products_db.model.entities.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
+
