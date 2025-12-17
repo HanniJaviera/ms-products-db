cdfadc137fca2823a0ffeefdaa97c21753bc3159
HanniJaviera <javiera.arc@hotmail.com>
2025-12-17 16:37:44 -0300

    ajuste de import

--- src/main/java/cl/duoc/ms_products_db/controller/ClimaController.java
+++ src/main/java/cl/duoc/ms_products_db/controller/ClimaController.java
@@ -1,8 +1,11 @@
 package cl.duoc.ms_products_db.controller;
 
-import cl.duoc.ms_products_db.service.ClimaService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import cl.duoc.ms_products_db.service.ClimaService;
+
 
 
 @RestController
@@ -14,7 +17,14 @@ public class ClimaController {
     private ClimaService climaService;
 
     @GetMapping
-    public String clima(@RequestParam String ciudad) {
-        return climaService.obtenerClima(ciudad);
+    public ResponseEntity<?> clima(@RequestParam String ciudad) {
+        try {
+            return ResponseEntity.ok(climaService.obtenerClima(ciudad));
+        } catch (Exception e) {
+            e.printStackTrace(); // IMPORTANTE para logs
+            return ResponseEntity
+                    .status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body("Servicio de clima no disponible");
+        }
     }
 }
